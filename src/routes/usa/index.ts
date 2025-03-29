0fe1d883348b25d44996690b31e9383224f7e132
Ashthetik <37787862+Ashthetik@users.noreply.github.com>
2025-03-29 22:19:54 +1100

    Include USA Terror Level
    TODO: https://www.dhs.gov/ntas-api-documentation

--- src/routes/usa/index.ts
+++ src/routes/usa/index.ts
@@ -3,28 +3,38 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/v1/ntas/usa", (req, res) => {
+    // TODO: Create a function to pull data from: https://www.dhs.gov/ntas-api-documentation
     res.status(200).json({
-        threat_level: "",
-        threat_no: "",
-        description: "",
+        threat_level: "No Current Threats",
+        threat_no: "0",
+        description: "There are no current advisories.",
         contacts: {
-            email: "hotline@nationalsecurity.gov.au",
+            emails: [
+                "SeeSay@hq.dhs.gov",
+                "tips.fbi.gov"
+            ],
             phone: {
-                "National Security Hotline": "1800 123 400",
-                "National Security SMS Hotline": "0498 562 549",
-                "National Security Toll-Free Hotline": "(+61) 1300 123 401",
+                "Federal Beauru of Intelligence": "1-800-CALL-FBI (1-800-225-5324)",
+                "Central Intelligence Agency": "(703) 613-1287",
+                "Office of Public Affairs": "(703) 482-0623",
                 "National Security TTY Hotline": "1800 234 889",
-                "Emergency Services": "000"
+                "Emergency Services": "911"
             },
-            reporting_link: "https://www.nationalsecurity.gov.au/what-can-i-do/report-suspicious-behaviour",
-            address: "National Security Hotline\nDepartment of Home Affairs, PO Box 25, Belconnen ACT 2616"
+            reporting_link: "https://www.dhs.gov/see-something-say-something",
+            addresses: [
+                "Central Intelligence Agency, Office of Public Affairs Washington, DC 20505",
+                "Field Offices can be found here: https://www.fbi.gov/contact-us/field-offices",
+                "FBI Headquarters, 935 Pennsylvania Avenue, NW Washington, D.C. 20535-0001 (202) 324-3000"
+            ],
         },
         guidelines: [
-            "https://www.nationalsecurity.gov.au/what-can-i-do/what-to-do-in-an-attack"
+            "https://www.fbi.gov/how-we-can-help-you/scams-and-safety",
+            "https://www.cia.gov/resources/",
+            "https://www.dhs.gov/see-something-say-something/about-campaign"
         ],
         news: [
-            "https://www.nationalsecurity.gov.au/news-media/national-security-campaign",
-            "https://www.nationalsecurity.gov.au/news-media/archive"
+            "https://www.fbi.gov/news",
+            "https://www.cia.gov/stories/"
         ]
     });
 })
